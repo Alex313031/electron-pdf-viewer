62a5b1b25f67c0942350e28ee3f8c3b47600ddd2
Alexander Frick <Alex313031@gmail.com>
2024-02-21 18:50:24 -0600

    add pdf.js build script

---
+++ build_pdfjs.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Copyright (c) 2024 Alex313031.
+
+YEL='\033[1;33m' # Yellow
+CYA='\033[1;96m' # Cyan
+RED='\033[1;31m' # Red
+GRE='\033[1;32m' # Green
+c0='\033[0m' # Reset Text
+bold='\033[1m' # Bold Text
+underline='\033[4m' # Underline Text
+
+# Error handling
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { "$@" || die "${RED}Failed $*"; }
+
+# --help
+displayHelp () {
+	printf "\n" &&
+	printf "${bold}${GRE}Script to build PDF.js.${c0}\n" &&
+	printf "${underline}${YEL}Usage:${c0} build.sh --build | --dist${c0}\n" &&
+	printf "--dist will copy the built files into ./app/lib${c0}\n" &&
+	printf "--help will show this help${c0}\n" &&
+	printf "\n"
+}
+case $1 in
+	--help) displayHelp; exit 0;;
+esac
+
+buildPDFjs () {
+	printf "\n" &&
+	printf "${bold}${GRE}Building PDF.js...${c0}\n" &&
+	printf "\n" &&
+
+	
+}
+case $1 in
+	--build) buildPDFjs; exit 0;;
+esac
+
+distPDFjs () {
+	printf "\n" &&
+	printf "${bold}${GRE}Building PDF.js...${c0}\n" &&
+	printf "\n" &&
+
+	cd ./pdf.js &&
+	npm install &&
+	npm run build
+}
+case $1 in
+	--dist) distPDFjs; exit 0;;
+esac
+
+printf "\n" &&
+printf "${bold}${GRE}Script to build PDF.js.${c0}\n" &&
+printf "${underline}${YEL}Usage:${c0} build.sh --build | --dist${c0}\n" &&
+printf "--dist will copy the built files into ./app/lib${c0}\n" &&
+printf "--help will show this help${c0}\n" &&
+printf "\n" &&
+tput sgr0
